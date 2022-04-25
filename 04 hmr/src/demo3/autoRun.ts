d42cbb5c20c31e402f641556073720d75e74c56b
Hencky <928856778@qq.com>
2022-04-25 21:34:04 +0800

    feat: 多个模块更新

---
+++ 04 hmr/src/demo3/autoRun.ts
@@ -0,0 +1,9 @@
+export function autoRun() {
+  let count = 0;
+
+  setInterval(() => {
+    let countEle = document.querySelector('#count')!;
+    count += 1;
+    countEle.innerHTML = String(count) + '';
+  }, 1000);
+}
